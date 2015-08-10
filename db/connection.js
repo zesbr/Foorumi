34ec2e37c2aec0dbd7823c070a2c6bcb950d30f6
zesbr <jesse_ruuth@hotmail.com>
2015-08-10 10:36:10 +0300

    Heroku configs

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,15 @@
 var Sequelize = require('sequelize');
 
+/*
 var sequelize = new Sequelize('foorumi', '', '', {
   dialect: 'sqlite',
   storage: 'db/database.sqlite'
+});
+*/
+
+var sequelize = new Sequelize('postgres://zrwsnhaascnwio:12qsIuu3ayLvRN6d8L6_afVepz@ec2-54-204-3-200.compute-1.amazonaws.com' {
+  dialect: 'postgres',
+  storage: 'postgres'
 });
 
 module.exports = {

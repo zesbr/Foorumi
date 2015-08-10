baf96d015de7228c037162ec6569a845df548fea
zesbr <jesse_ruuth@hotmail.com>
2015-08-10 18:44:43 +0300

    Heroku config

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ var sequelize = new Sequelize('foorumi', '', '', {
 });
 */
 
-var sequelize = new Sequelize('postgres://zrwsnhaascnwio:12qsIuu3ayLvRN6d8L6_afVepz@ec2-54-204-3-200.compute-1.amazonaws.com:5432/d82lcg0akajalh' {
+var sequelize = new Sequelize('postgres://zrwsnhaascnwio:12qsIuu3ayLvRN6d8L6_afVepz@ec2-54-204-3-200.compute-1.amazonaws.com:5432/d82lcg0akajalh', {
   dialect: 'postgres',
   storage: 'postgres'
 });
